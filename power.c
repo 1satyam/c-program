5c1f8e64053d1797b366d2e4b89eac33c13772f6
1satyam <94980142+1satyam@users.noreply.github.com>
2022-11-29 15:28:17 +0000

    Add files via upload

---
+++ power.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+ 
+ int main()
+ {
+    int b,p,result=1;
+    int i=1;
+    printf("enter the base number ");
+    scanf("%d",&b);
+    printf("Enter the power of the base");
+    scanf("%d",&p);
+    while(i<=p)
+    {
+        result=result*b;
+        i++;
+    }
+        printf("the number is %d",result);
+    return 0;
+ }
