f1070f2e472c559caec3b75d3ccef1fe875ecada
1satyam <94980142+1satyam@users.noreply.github.com>
2022-11-29 16:21:25 +0000

    Add files via upload

---
+++ equal.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int m,n;
+    printf("Enter the first number");
+    scanf("%d",&m);
+    printf("Enter the second number");
+    scanf("%d",&n);
+    if(m==n)
+    {
+        printf("the numbers are equal");
+    }
+    else
+    {
+          printf("The numbers are not equal");
+    }
+    return 0;
+}
