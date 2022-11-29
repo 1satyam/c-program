45245035dfc9fd99b109db2bb2754a850254b62f
1satyam <94980142+1satyam@users.noreply.github.com>
2022-11-29 15:57:40 +0000

    Update sum.c

--- sum.c
+++ sum.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
-/*int main()
-{
- int a=3, b=5;
- printf("the sum of two number is %d.",a+b);
- return 0;
-}*/
+
 int main()
 { int a, b;
     printf("enter the value of a and b");
     scanf("%d%d",&a,&b);
     printf("the sum of two numbers is %d",a+b);
-    }
+    return 0;
+}
