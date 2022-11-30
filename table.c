16bdb485926a47cf5362013e1ba1cca1940b4e7f
1satyam <94980142+1satyam@users.noreply.github.com>
2022-11-30 21:59:15 +0530

    Add files via upload

---
+++ table.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int num, i=1;
+    printf("enter the table number ");
+    scanf("%d",&num);
+    while(i<=10)
+    {
+       printf("%d\n",num*i);
+       i++;    
+    }
+    return 0;
+}
